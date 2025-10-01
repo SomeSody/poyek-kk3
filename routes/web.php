dc3f4269f9b7e5a6266dcd6f0d1f8ce215198401
nabilla24si-ship-it <nabilla24sii@pcr.ac.id>
2025-10-01 10:47:36 +0700

    tes44

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/anggota', function () {
+Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anggota', function () {
+    return view('wanggota');
+});
+
